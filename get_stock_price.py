56e4006a483b1d5aae674738d14e9da4de385e6c
shunchama <153877748+shunchama@users.noreply.github.com>
2025-04-30 02:38:22 +0900

    Add files via upload

--- get_stock_price.py
+++ get_stock_price.py
@@ -1,45 +1,77 @@
-import datetime
-import pandas as pd
-import numpy as np
+import subprocess
+import sys
+required_packages = ["yfinance", "matplotlib", "pandas"]
+for package in required_packages:
+    try:
+        __import__(package)
+    except ImportError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
 import yfinance as yf
-import matplotlib.pyplot as plt
+#import datetime
+#mport pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+
+#define lists
+index_list=[
+    "^N225",#日経平均
+    "JPY=X"#ドル円
+]
+owning_list=[
+    "4755.T","7201.T","9432.T","9501.T","141A.T"
+]
+check_list=[
+    "7011.T","7012.T","7013.T","7014.T"
+]
+
 
 def get_ticker_info(ticker):
     stock=yf.Ticker(ticker)
-    return stock.info
+    if stock!=None:
+        return stock.info
+    else:
+        print("[err] ticker_info not found")
+        return None
 
 def get_cur_price(ticker):
     info=get_ticker_info(ticker)
     if info.get("regularMarketPrice")!=None:
         return info.get("regularMarketPrice")
     else:
-        return "not found"
+        print("[err] cur_price not found")
+        return None
 
 def get_name(ticker):
     info=get_ticker_info(ticker)
     if(info.get("longName")!=None):
         return info.get("longName") or info.get("shortName")
     else:
-        return "not found"
+        print("[err] name not found")
+        return None
 
 def get_prev_close(ticker):
     info=get_ticker_info(ticker)
-    if(info.get(previousClose)!=None):
+    if(info.get("previousClose")!=None):
         return info.get("previousClose")
     else:
-        return "not found"
+        print("[err] prev_close not found")
+        return None
 
 def get_price(ticker):
-    info=get_ticker_info(ticker)
+    cur_price=0.0
+    prev_close=0.0
+    change=0.0
+    change_percentage=0.0
+    #info=get_ticker_info(ticker)
     cur_price=get_cur_price(ticker)
-    prev_close=info.get("previousClose")
+    prev_close=get_prev_close(ticker)
     name=get_name(ticker)
-    
-    if cur_price==None or prev_close==None:
-        return None,None,None,None
-    
-    change=cur_price-prev_close
-    change_percentage=(change/prev_close)*100
+
+    if cur_price!=None and prev_close!=None:
+        change=cur_price-prev_close
+        change_percentage=(change/prev_close)*100
     return name,cur_price,change,change_percentage
 
 def print_price(ticker_list):
@@ -59,21 +91,53 @@ def print_index(index_list):
     print_price(index_list)
     print("*********************************")
 
-def main():
-    index_list=[
-        "^N225",#日経平均
-        "JPY=X"#ドル円
-    ]
-    owning_list=[
-        "4755.T","7201.T","9432.T","9501.T"
-    ]
-    check_list=[
-        "7011.T","7012.T","7013.T","7014.T"
-    ]
-    
+def get_volume(ticker):
+    info=get_ticker_info(ticker)
+    if(info.get("volume")!=None):
+        return info.get("volume")
+    else:
+        return "[err]volume not found"
+
+def print_list():
     print_index(index_list)
     print_price(owning_list)
     print_price(check_list)
+
+def read_list():
+    print("index_list={}".format(index_list))
+    print("owning_list={}".format(owning_list))
+    print("check_list={}".format(check_list))
+
+def wait_command():
+    command=[
+        "print_list(defined_list)",
+        "print_index",
+        "get+(in:ticker).T",
+        "read_list",
+        "exit"
+    ]
+    print("*********************************\ncommand_list: {}\n*********************************".format(command))
+    while True:
+        print("command: ",end="")
+        c=input()
+        if c=="print_list":
+            print_list()
+        elif c=="print_index":
+            print_index(index_list)
+        elif c=="get":
+            print("enter the ticker: ",end="")
+            num=input()+".T"
+            name=[num]
+            print_price(name)
+        elif c=="read_list":
+            read_list()
+        elif c=="exit":
+            return
+        else:
+            print("[err]command not found, plz retype")
+
+def main():
+    wait_command()
 
 if __name__=="__main__":
     main()
